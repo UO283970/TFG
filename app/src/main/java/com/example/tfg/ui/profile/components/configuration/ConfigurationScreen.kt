5c3006b9474b63891e11b402c6ca35eaf2ec98b0
UO283970 <98962779+UO283970@users.noreply.github.com>
2025-04-17 13:20:47 +0200

    Logout y borrado del usuario.

--- app/src/main/java/com/example/tfg/ui/profile/components/configuration/ConfigurationScreen.kt
+++ app/src/main/java/com/example/tfg/ui/profile/components/configuration/ConfigurationScreen.kt
@@ -5,15 +5,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowForward
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -23,7 +21,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.tfg.R
-import com.example.tfg.model.ConfigurationItems
+import com.example.tfg.ui.common.navHost.HomeRoutesItems
 import com.example.tfg.ui.lists.listDetails.components.TopDetailsListBar
 import com.example.tfg.ui.theme.TFGTheme
 
@@ -33,16 +31,22 @@ fun ConfigurationScreen(
     navigateTo: (route: String) -> Unit,
     viewModel: ConfigurationScreenViewModel = hiltViewModel()
 ) {
+    LaunchedEffect(viewModel.confState.toLogin) {
+        if(viewModel.confState.toLogin){
+            navigateTo(HomeRoutesItems.LoginScreen.route)
+        }
+    }
+
     TFGTheme {
         Scaffold(topBar = {
             TopDetailsListBar(returnToPreviosScreen, stringResource(R.string.profile_configuration))
         }) { innerPadding ->
             Column(Modifier.padding(innerPadding)) {
-                LazyColumn {
-                    items(ConfigurationItems.entries) {
-                        HorizontalDivider()
-                        ConfigurationRow(navigateTo, it.stringResource, it.mainIcon, it.route)
-                    }
+                Column {
+                    ConfigurationRow({ navigateTo("") }, stringResource(R.string.profile_configuration_security), R.drawable.security_icon)
+                    ConfigurationRow({ navigateTo("") }, stringResource(R.string.profile_configuration_notifications), R.drawable.notification_icon)
+                    ConfigurationRow({viewModel.deleteAccount()}, stringResource(R.string.profile_configuration_delete_account), R.drawable.delete_icon)
+                    ConfigurationRow({viewModel.logout()}, stringResource(R.string.profile_configuration_logout), R.drawable.logout_icon)
                 }
 
             }
@@ -52,20 +56,19 @@ fun ConfigurationScreen(
 
 @Composable
 private fun ConfigurationRow(
-    navigateTo: (String) -> Unit,
-    mainTittle: Int,
-    mainIcon: Int,
-    mainRoute: String
+    mainOnClick: () -> Unit,
+    mainTittle: String,
+    mainIcon: Int
 ) {
     Row(
         modifier = Modifier
-            .clickable { navigateTo(mainRoute) }
+            .clickable { mainOnClick() }
             .padding(top = 10.dp, start = 5.dp, end = 5.dp, bottom = 10.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Icon(painterResource(mainIcon), stringResource(mainTittle), modifier = Modifier.size(28.dp))
+        Icon(painterResource(mainIcon), mainTittle, modifier = Modifier.size(24.dp))
         Text(
-            stringResource(mainTittle),
+            mainTittle,
             modifier = Modifier.weight(1f),
             fontWeight = FontWeight.SemiBold,
             fontSize = 24.sp
--- app/src/main/java/com/example/tfg/ui/profile/components/configuration/ConfigurationScreenViewModel.kt
+++ app/src/main/java/com/example/tfg/ui/profile/components/configuration/ConfigurationScreenViewModel.kt
@@ -1,12 +1,43 @@
 package com.example.tfg.ui.profile.components.configuration
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.tfg.model.security.TokenRepository
+import com.example.tfg.repository.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+data class ConfigurationState(
+    var toLogin: Boolean = false
+)
+
 @HiltViewModel
-class ConfigurationScreenViewModel @Inject constructor(): ViewModel() {
+class ConfigurationScreenViewModel @Inject constructor(val userRepository: UserRepository, val tokenRepository: TokenRepository): ViewModel() {
 
+    var confState by mutableStateOf(ConfigurationState())
 
+    fun logout(){
+        viewModelScope.launch {
+            val response = userRepository.logout()
+            if(response != null && response != ""){
+                tokenRepository.clearTokens()
+                confState = confState.copy(toLogin = true)
+            }
+        }
+    }
+
+    fun deleteAccount(){
+        viewModelScope.launch {
+            val response = userRepository.deleteUser()
+            if(response != null && response){
+                tokenRepository.clearTokens()
+                confState = confState.copy(toLogin = true)
+            }
+        }
+    }
 
 }
